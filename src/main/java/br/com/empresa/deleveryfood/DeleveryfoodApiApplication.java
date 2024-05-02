f589b00c13f6b67bb3c36856d4d46e35365231c9
Francisco de Assis <a124fr@gmail.com>
2024-05-01 22:29:29 -0300

    Projeto inicial

---
+++ src/main/java/br/com/empresa/deleveryfood/DeleveryfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.empresa.deleveryfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleveryfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleveryfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/empresa/deleveryfood/DeleveryfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresa.deleveryfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleveryfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
